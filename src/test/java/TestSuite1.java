b6c63f1500006b62e6d5cff3d70954491319e533
Nikita Kuznetsov <nikita_kuznetsov@list.ru>
2018-04-08 22:36:52 +0300

    change encoding

--- src/test/java/TestSuite1.java
+++ src/test/java/TestSuite1.java
@@ -1,4 +1,4 @@
-import org.openqa.selenium.By;
+п»їimport org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -16,19 +16,19 @@ public class TestSuite1 extends BaseRunner {
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
-        driver.findElement(By.xpath("//input[@id='']")).sendKeys("Тинькофф Мобайл");
+        driver.findElement(By.xpath("//input[@id='']")).sendKeys("РўРёРЅСЊРєРѕС„С„ РњРѕР±Р°Р№Р»");
         driver.findElement(By.xpath("//div[@id='search-and-pay-container']/div[2]/div[2]/div/form/div[3]/div/div/div/div[2]/div/div/div/div/div/div/div/div")).click();
         driver.findElement(By.xpath("//form/div[2]/div/div/div/div/div/div/div/div/label")).click();
         driver.findElement(By.xpath("//input[@id='']")).clear();
         driver.findElement(By.xpath("//input[@id='']")).sendKeys("5");
         driver.findElement(By.xpath("//button")).click();
         driver.findElement(By.xpath("//div[5]/div/div[2]/div/div/div")).click();
-        assertEquals(driver.findElement(By.xpath("//div[2]/div/div/div/div/form/div/div/div[2]")).getText(), "Поле обязательное");
-        assertEquals(driver.findElement(By.xpath("//form/div[2]/div/div/div/div/div/div/div/div[2]")).getText(), "Минимум — 10 \u20BD");
+        assertEquals(driver.findElement(By.xpath("//div[2]/div/div/div/div/form/div/div/div[2]")).getText(), "РџРѕР»Рµ РѕР±СЏР·Р°С‚РµР»СЊРЅРѕРµ");
+        assertEquals(driver.findElement(By.xpath("//form/div[2]/div/div/div/div/div/div/div/div[2]")).getText(), "РњРёРЅРёРјСѓРј вЂ” 10 \u20BD");
         for (int second = 0; ; second++) {
             if (second >= 60) fail("timeout");
             try {
-                if ("Оплатить мобильную связь".equals(driver.getTitle())) break;
+                if ("РћРїР»Р°С‚РёС‚СЊ РјРѕР±РёР»СЊРЅСѓСЋ СЃРІСЏР·СЊ".equals(driver.getTitle())) break;
                 Thread.sleep(1000);
             } catch(Exception ex) {
                 ex.printStackTrace();
